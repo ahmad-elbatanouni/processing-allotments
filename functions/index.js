999b6d105030647e67fb1a6f64f1e52a9c0d1fbf
Ahmad <egyptstyles1@gmail.com>
2018-10-16 02:14:00 +0200

    added all the functionalities asked for

---
+++ functions/index.js
@@ -0,0 +1,62 @@
+const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+var serviceAccount = require("path/to/secret_key.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://processing-allotments.firebaseio.com"
+});
+
+// for contacting the sendinblue API
+var request = require("request");
+
+var options = { 
+    method: 'POST',
+    url: 'https://api.sendinblue.com/v3/emailCampaigns/1/sendTest',
+    body: { emailTo: [] },
+    headers: {
+        'api-key': 'SECRET_KEY'
+    },
+    json: true 
+};
+
+
+const db = admin.database();
+var ref, email = "NO_MAIL";
+
+
+exports.new_allotment = functions.database.ref('/allotments/{allotment_id}')
+    .onCreate((snapshot, context) => {
+      
+        // Grab the newly added document
+        const original = snapshot.val();
+
+        // Check for files in the added document and update their relative paths
+        if (original.files)
+            original.files.forEach(file => {
+                ref = db.ref("/files/" + file + "/CR");
+                ref.set({status: "Given"});
+            });
+
+        
+        
+        if (original.person)
+            if(original.person.emailAddress) {
+                // Grab the email
+                email = original.person.emailAddress;
+
+                // append it to the array of recepients 
+                options.body.emailTo.push(email);
+
+                request(options, function (error, response, body) {
+                    if (error) throw new Error(error);
+                });
+            }
+
+
+
+        
+        // the function MUST return a promise, so I added the same email to the new node
+        return snapshot.ref.child('person').child('emailAddress').set(email);
+    });
